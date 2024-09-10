1b2a91852361d0812d1f8db8a2eb66edab522f2b
Luana Aguilar <151654138+LuanaAguilar2@users.noreply.github.com>
2024-09-10 14:29:29 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,40 @@
+perguntas = [
+    {
+        "O poder da fruta Ope Ope no Mi pertence a qual personagem?":
+            ["Donquixote  Doflamingo",
+                "Mokey D. Luffy",
+                "Trafalgar Law",
+                "Eustass Kid"
+            ]
+    },
+
+    {
+        "Qual o nome da ilha que guarda o tesouro one piece?":
+            ["Laugh Tale",
+                "Ruluka Island",
+                "Hyokaido",
+                "Green Bit",
+            ]
+    },
+    {
+        "Qual o nome do navio do considerado rei dos piratas?":
+            ["Moby Dick",
+                "Oro Jackson",
+                "Going Merry",
+                "Red Force"]
+    },
+    {
+        "Qual foi o primeiro shichibukais derrotado por Luffy?":
+            ["Crocodile",
+                "Gecko Moriah",
+                "Donquixote doflamingo",
+                "Bartholomew kuma"]
+    },
+    {
+        "Qual foi o yonkous que morreu na guerra de marineford?":
+            ["Barba negra",
+                "Shanks",
+                "Big mom",
+                "Barba branca"]
+    }
+]
