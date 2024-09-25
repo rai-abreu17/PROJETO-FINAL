43af13d2135b994c10c59ef7aaf6cdb1af5f4cd9
Luana Aguilar <lalvesaguilar@gmail.com>
2024-09-25 14:05:08 -0300

    fases_adicionadas

--- Script.js
+++ Script.js
@@ -4,6 +4,193 @@
 // teste
 
 const quizData = [
+    //faceis 
+    {
+        question: "Qual foi o primeiro Shichibukai derrotado por Luffy?",//fácil
+        answers: [
+            { text: "Crocodile", correct: true },
+            { text: "Gecko Moriah", correct: false },
+            { text: "Donquixote Doflamingo", correct: false },
+            { text: "Bartholomew Kuma", correct: false }
+        ],
+        //fazer lá embaixo if level == 1 então mostrar bolinha  
+        level: 1
+    },
+    {
+        question: "O Itadori é hospedeiro de qual desses?", //fácil
+        answers: [
+            { text: "Kurama", correct: false },
+            { text: "Ryomen Sukuna", correct: true },
+            { text: "Momoshiki Otsutsuki", correct: false },
+            { text: "Jinchuuriki", correct: false }
+
+        ],
+        level: 1
+    },
+
+    {
+        question: "Quem é o personagem principal de Demon Slayer? ",
+        answers: [
+            { text: "Tomioka", correct: false },
+            { text: "Zenitsu", correct: false },
+            { text: "Tanjiro", correct: true },
+            { text: "Nezuko", correct: false }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual o nome da técnica que permite ao usuário transferir sua própria vida para outra pessoa? ",
+        answers: [
+            { text: "Izanagi", correct: false },
+            { text: "Izanami", correct: false },
+            { text: "Shiki Fujin", correct: true },
+            { text: "Kuchiyose: No Jutsu", correct: false }
+        ],
+        level: 1
+    },
+    {
+        question: "Qual foi o Yonkou que morreu na guerra de Marineford?",//Fácil
+        answers: [
+            { text: "Barba Negra", correct: false },
+            { text: "Shanks", correct: false },
+            { text: "Big Mom", correct: false },
+            { text: "Barba Branca", correct: true }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual desses é o pai de Megumi Fushiguro?",
+        answers: [
+            { text: "Toji", correct: true },
+            { text: "Satoru Gojo", correct: false },
+            { text: "Yuta Okkotsu", correct: false },
+            { text: "Gabimaru", correct: false }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual destes Jinchuuriki não foi capturado pela Akatsuki? ",
+        answers: [
+            { text: "Gaara", correct: false },
+            { text: " Yagura", correct: false },
+            { text: "Roshi", correct: true },
+            { text: "Han", correct: false }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual jutsu de Naruto Uzumaki foi inspirado em um animal? ",
+        answers: [
+            { text: "Rasengan ", correct: true },
+            { text: "Rasenshuriken", correct: false },
+            { text: "Onda Trovejante", correct: false },
+            { text: "Multi-clones das Sombras", correct: false }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual é o Hashira mais forte?",
+        answers: [
+            { text: "Rengoku", correct: false },
+            { text: "Tomioka", correct: false },
+            { text: "Obanai", correct: false },
+            { text: "Gyomei Himejima", correct: true }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Qual é o nome da técnica que Goku usou para derrotar o Majin Boo?",
+        answers: [
+            { text: "Genki Dama", correct: true },
+            { text: "Kamehameha", correct: false },
+            { text: "Kaio-ken", correct: false },
+            { text: "Super Saiyajin 3", correct: false }
+        ],
+        level: 1
+    },
+
+    {
+        question: "Quantos anos se passaram entre o fim da saga de Majin Boo e o começo do Torneio do Poder?",
+        answers: [
+            { text: "5 Anos", correct: false },
+            { text: "3 Anos", correct: false },
+            { text: "10 Anos", correct: true },
+            { text: "1 ano", correct: false }
+        ],
+        level: 1
+    },
+
+    //media
+    {
+        question: "Qual o nome da maldição que Yuta Okkotsu carrega? ",
+        answers: [
+            { text: "Rika Orimoto", correct: true },
+            { text: "Kyubi", correct: false },
+            { text: "Hanami", correct: false },
+            { text: "Dagon", correct: false }
+        ],
+        level: 2
+    },
+    {
+        question: "Qual o nome do navio do considerado rei dos piratas?",
+        answers: [
+            { text: "Moby Dick", correct: false },
+            { text: "Oro Jackson", correct: true },
+            { text: "Going Merry", correct: false },
+            { text: "Red Force", correct: false }
+        ],
+        level: 2
+    },
+    {
+        question: "Qual é o nome do planeta onde Goku treinou com o Senhor Kaioh? ",
+        answers: [
+            { text: " Namekusei", correct: false },
+            { text: "Planeta Vegeta", correct: false },
+            { text: "Planeta Kaioh", correct: true },
+            { text: "Terra", correct: false }
+        ],
+        level: 2
+    },
+    {
+        question: "Qual é a técnica amaldiçoada de Satoru Gojo? ",
+        answers: [
+            { text: "Controle de Fogo", correct: false },
+            { text: "Expansão de Domínio: Infinito Vazio", correct: true },
+            { text: "Manipulação de Sombras", correct: false },
+            { text: "Controle de Raio", correct: false }
+        ],
+        level: 2
+    },
+
+    {
+        question: "Qual o nome do mestre dos Hashiras? ",
+        answers: [
+            { text: "Muzan", correct: false },
+            { text: "Urokodaki", correct: true },
+            { text: "Genya", correct: false },
+            { text: "Kanroji", correct: false }
+        ],
+        level: 2
+    },
+
+    //dificil
+    {
+        question: "Qual é a técnica de expansão de domínio de Satoru Gojo?  ",
+        answers: [
+            { text: "Jardim dos Espinhos", correct: false },
+            { text: "Mar de Areia e Sangue", correct: false },
+            { text: "Céu Sem Limites", correct: false },
+            { text: "Infinito Vazio", correct: true }
+        ],
+        level: 3
+    },
+
     {
         question: "O poder da fruta Ope Ope no Mi pertence a qual personagem?",
         answers: [
@@ -11,7 +198,29 @@ const quizData = [
             { text: "Monkey D. Luffy", correct: false },
             { text: "Trafalgar Law", correct: true },
             { text: "Eustass Kid", correct: false }
-        ]
+        ],
+        level: 3
+    },
+    {
+        question: "Qual é o nome do anjo que serve como mentor de Goku e Vegeta durante o Torneio do Poder?",
+        answers: [
+            { text: "Whis", correct: true },
+            { text: "Vados", correct: false },
+            { text: "Beerus", correct: false },
+            { text: "Kami", correct: false }
+        ],
+        level: 3
+    },
+
+    {
+        question: "Quantos irmão Tanjiro tinha? ",
+        answers: [
+            { text: "3 irmãos", correct: false },
+            { text: "8 irmãos", correct: false },
+            { text: "Nenhum", correct: false },
+            { text: "5 irmãos", correct: true }
+        ],
+        level: 3
     },
 
     {
@@ -21,43 +230,56 @@ const quizData = [
             { text: "Ruluka Island", correct: false },
             { text: "Hyokaido", correct: false },
             { text: "Green Bit", correct: false }
-        ]
+        ],
+        level: 3
     },
+
     {
-        question: "Qual o nome do navio do considerado rei dos piratas?",
+        question: "Qual é o Lua Superior mais forte? ",
         answers: [
-            { text: "Moby Dick", correct: false },
-            { text: "Oro Jackson", correct: true },
-            { text: "Going Merry", correct: false },
-            { text: "Red Force", correct: false }
-        ]
+            { text: "Kokushibou", correct: true },
+            { text: "Doma", correct: false },
+            { text: "Akaza", correct: false },
+            { text: "Kaigaku", correct: false }
+        ],
+        level: 3
     },
+
     {
-        question: "Qual foi o primeiro Shichibukai derrotado por Luffy?",
+        question: "Qual é a técnica que permite a Goku e outros Saiyajins transformarem-se em Super Saiyajin Blue?",
         answers: [
-            { text: "Crocodile", correct: true },
-            { text: "Gecko Moriah", correct: false },
-            { text: "Donquixote Doflamingo", correct: false },
-            { text: "Bartholomew Kuma", correct: false }
-        ]
+            { text: "Kamehameha", correct: false },
+            { text: "Super Saiyajin Deus", correct: true },
+            { text: "Kaio-ken", correct: false },
+            { text: "Genki Dama", correct: false }
+        ],
+        level: 3
     },
+
     {
-        question: "Qual foi o Yonkou que morreu na guerra de Marineford?",
+        question: "Qual é o nome do clã que possui a Técnica Amaldiçoada Manipulação das Dez Sombras, utilizada por Megumi Fushiguro em Jujutsu Kaisen ? ",
         answers: [
-            { text: "Barba Negra", correct: false },
-            { text: "Shanks", correct: false },
-            { text: "Big Mom", correct: false },
-            { text: "Barba Branca", correct: true }
-        ]
-    },
-    {
-        question: "O Itadori é hospedeiro de qual desses?",
-        answers: [
-            { text: "Kurama", correct: false },
-            { text: "Ryomen Sukuna", correct: true }
-        ]
+            { text: "Clã Kamo ", correct: false },
+            { text: "Clã Gojo ", correct: false },
+            { text: "Clã Zen'in ", correct: true },
+            { text: "Clã Inumaki", correct: false }
+        ],
+        level: 3
     }
 ];
+//definir qual o nivel da pergunta 
+//if (level == 1) {
+
+//mostrar bolinha verde  
+//}
+/*elseif (level == 2){
+    //mostrar bolinha laranja 
+}
+else {
+    //mostrar bolinha vermelha  
+}*/
+
+//let nome = prompt("Bem vindo ao Quiz, qual seu nome? ")
 
 let currentQuestionIndex = 0; // Índice para rastrear a pergunta atual
 
@@ -134,13 +356,3 @@ loadQuestion();
 
 
 
-/*
-   <div id="quiz">
-        <p id="idPergunta"> pergunta </p>
-        <div id="idButtonsContainer">
-            <button className="answer"></button>
-            <button className="answer"></button>
-            <button className="answer"></button>
-        </div>
-    </div>
-*/
